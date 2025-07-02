92fd351d2486df6a645a29976f2639688e327461
David Jeusette <jeusette.david@gmail.com>
2025-07-03 00:19:13 +0200

    Add ex_doc to the list of deps

--- mix.exs
+++ mix.exs
@@ -41,7 +41,8 @@ defmodule GqlCase.MixProject do
   defp deps do
     [
       {:plug, "~> 1.18"},
-      {:absinthe, "~> 1.7"}
+      {:absinthe, "~> 1.7"},
+      {:ex_doc, "~> 0.38", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
